3160b964a98f0321906396a40b263a61792d24fa
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2024-04-24 23:03:00 +0100

    Database connected

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('DB connected succesfully!'));
+
+const port = 5000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on ${port}...`);
 });
